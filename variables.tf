e609bb0fd15dfde4d939792b0fb84a1b92215462
shivalkarrahul <rahulshivalkar@rediffmail.com>
2022-05-20 07:47:19 +0000

    Follow Naming Convention

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable "instance_ami" {
+variable "instance_1_ami" {
   description = "Value of the AMI ID for the EC2 instance"
   type        = string
   default     = "ami-005de95e8ff495156"
 }
 
-variable "instance_type" {
+variable "instance_1_type" {
   description = "Value of the Instance Type for the EC2 instance"
   type        = string
   default     = "t2.micro"
 }
 
-variable "instance_name" {
+variable "instance_1_name" {
   description = "Value of the Name Tag for the EC2 instance"
   type        = string
   default     = "instance-1"
