b79c581b8333d93eb60cab6ce89da89a3fbadee5
shivalkarrahul <rahulshivalkar@rediffmail.com>
2022-05-20 05:53:24 +0000

    corrected variable assignment

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "my_example" {
   ami           = var.instance_ami
   instance_type = var.instance_type
   tags = {
-    Name = "var.instance_name"
+    Name = var.instance_name
   }
 }
