6bc22e6264dcf3579781cfba9f1ef199e6b4f6a1
shivalkarrahul <rahulshivalkar@rediffmail.com>
2022-05-23 06:55:05 +0000

    Take advantage of built-in functions

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
+resource "aws_key_pair" "terraform_best_practices_demo" {
+  key_name   = "terraform-best-practices-demo-key"
+  public_key = file("/home/ubuntu/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "instance_1" {
   ami           = var.instance_1_ami
   instance_type = var.instance_1_type
   tags = {
     Name = var.instance_1_name
   }
+  key_name = "${aws_key_pair.terraform_best_practices_demo.key_name}"
 }
 
 resource "aws_instance" "instance_2" {
@@ -16,6 +22,7 @@ resource "aws_instance" "instance_2" {
     command    = "echo The IP address of the Server is ${self.private_ip}"
     on_failure = continue
   }
+  key_name = "${aws_key_pair.terraform_best_practices_demo.key_name}"
 }
 
 module "website_s3_bucket_1" {
